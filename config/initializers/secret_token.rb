e0cab5d7b99b7adb204771adb4803a792f8c86a4
Scooter Schneider <scooter@kona-aloha.com>
2011-08-26 19:06:17 -0400

    initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '65393779a22c8ce86caf7062eb9426ceea3086c83c5adce9a31c9d668d735d21e0069f7111e682c1da35e06eefe0b2c343dd8a408f4c6fcd66dbfb673994df40'
